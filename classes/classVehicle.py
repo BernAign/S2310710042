27be201a5bd6346058b9fbd180a252119c855752
Barbarius <aigner.bernhard@gmx.at>
2024-01-05 21:39:21 +0100

    added args to class

--- classes/classVehicle.py
+++ classes/classVehicle.py
@@ -1,26 +1,25 @@
-class vehicle:
+class Vehicle:
+    amount = 0
+       # constructor
+    def __init__(self, args):
+        self.mass = args.mass
+        self.friction = args.friction
+        self.velocity = args.velocity
+        Vehicle.amount += 1
+        print(args.mass)
 
-#constructor
-    def __init__(self, mass, friction, velocity):
-        self.mass = mass
-        self.friction = friction
-        self.velocity = velocity
-        vehicle.amount += 1
-
-#destructor
+    # destructor
     def __del__(self):
         print("vehicle deleted!")
-        vehicle.amount -= 1
+        Vehicle.amount -= 1
 
-#string method
+    # string method
     def __str__(self):
-        return("Mass: {}, Friction: {}, Velocity: {}".format(self.mass, self.friction, self.velocity))
+        return "Mass: {}, Friction: {}, Velocity: {}".format(self.mass, self.friction, self.velocity)
 
-#decelaration method
-    def getBreakDistance(args):
+    # deceleration method
+    def get_brake_distance(self, args):
         if args.pdfFileName == "blub":
-            print (1)
-        if args.pdfFilePath == r"C:\Users\aigne\OneDrive\Dokumente\Git_Repo_FH\S2310710042" :
+            print(1)
+        if args.pdfFilePath == r"C:\Users\aigne\OneDrive\Dokumente\Git_Repo_FH\S2310710042":
             print("passed  " + args.pdfFilePath)
-
-    
