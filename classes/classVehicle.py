1e6e9276a535fed679d386bf28c427a5aa0e5de0
Barbarius <aigner.bernhard@gmx.at>
2024-01-11 00:40:44 +0100

    added roadtype

--- classes/classVehicle.py
+++ classes/classVehicle.py
@@ -1,12 +1,15 @@
+from classes import constants
+
 class Vehicle:
+    
     amount = 0
-       # constructor
+       
+    # constructor
     def __init__(self, args):
         self.mass = args.mass
-        self.friction = args.friction
+        self.friction = self.set_roadtype(args)
         self.velocity = args.velocity
         Vehicle.amount += 1
-        print(args.mass)
 
     # destructor
     def __del__(self):
@@ -18,8 +21,27 @@ class Vehicle:
         return "Mass: {}, Friction: {}, Velocity: {}".format(self.mass, self.friction, self.velocity)
 
     # deceleration method
-    def get_brake_distance(self, args):
-        if args.pdfFileName == "blub":
-            print(1)
-        if args.pdfFilePath == r"C:\Users\aigne\OneDrive\Dokumente\Git_Repo_FH\S2310710042":
-            print("passed  " + args.pdfFilePath)
+    def get_brake_distance(self):
+        if self.mass == 1500.0:
+            print(self.friction)
+        # if args.pdfFilePath == r"C:\Users\aigne\OneDrive\Dokumente\Git_Repo_FH\S2310710042":
+        #     print("passed  " + args.pdfFilePath)
+        
+    def set_roadtype(self, args):
+        if(args.concrete and not args.wet):
+            return constants.CONCRETE_DRY
+        elif(args.concrete and args.wet):
+            return constants.CONCRETE_WET
+        elif(args.ice and not args.wet):
+            return constants.ICE_DRY
+        elif(args.ice and args.wet):
+            return constants.ICE_WET
+        elif(args.water):
+            return constants.WATER
+        elif(args.gravel):
+            return constants.GRAVEL
+        elif(args.sand):
+            return constants.SAND
+
+
+  
