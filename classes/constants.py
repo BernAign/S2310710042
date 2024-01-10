00c69d52fc45686409d3de332aa1c3b58ef250ea
Barbarius <aigner.bernhard@gmx.at>
2024-01-11 00:41:17 +0100

    defined constants friction coefficient

---
+++ classes/constants.py
@@ -0,0 +1,9 @@
+#constants friction coefficient
+CONCRETE_DRY =      0.65
+CONCRETE_WET =      0.35
+ICE_WET =           0.15
+ICE_DRY =           0.1
+WATER =             0.1
+GRAVEL =            0.35
+SAND =              0.3
+    
